30099b10e76651aa73960b91c3b05f6b5de1d78f
vargis.mithun <varghese.mithun93@gmail.com>
2025-09-30 16:50:54 +0530

    Set up client

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 
 import type {Request, Response} from 'express'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
 
 const app = express();
 
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("Hello World!!!!")
+    res.send({
+        key: process.env.OPEN_AI_KEY
+    })
 })
 
 
