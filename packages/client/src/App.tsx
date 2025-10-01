45770ab8a561ee57df882a17eee0ecf0ae812d9e
vargis.mithun <varghese.mithun93@gmail.com>
2025-10-01 23:58:19 +0530

    Set up open AI

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,22 +1,21 @@
-import { useEffect, useState } from "react";
-import { Button } from "./components/ui/button";
+import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  
-  const [message, setMessage] = useState('');
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch('/api/test')
-    .then(res => res.json())
-    .then(data => setMessage(data.message))
-  }, []);
+   useEffect(() => {
+      fetch('/api/test')
+         .then((res) => res.json())
+         .then((data) => setMessage(data.message));
+   }, []);
 
-  return (
-    <div className="p-5">
-    <p className="font-bold text-4xl pb-3">{message}</p>
-    <Button className="rounded-b-md">Click me</Button>
-    </div>
-  )
+   return (
+      <div className="p-5">
+         <p className="font-bold text-4xl pb-3">{message}</p>
+         <Button className="rounded-b-md">Click me</Button>
+      </div>
+   );
 }
 
-export default App 
+export default App;
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,24 +3,45 @@ import express from 'express';
 import type {Request, Response} from 'express'
 import dotenv from 'dotenv';
 
-dotenv.config();
+import OpenAI from 'openai'
 
+dotenv.config();
+const client = new OpenAI({
+    apiKey: process.env.OPEN_AI_KEY,
+    organization: 
+});
 
 
 const app = express();
 
+app.use(express.json())
 
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        key: process.env.OPEN_AI_KEY
-    })
-})
 
 app.get('/api/test', (req: Request, res: Response) => {
     res.send({
-        message: "hello World"
+        key: process.env.OPEN_AI_KEY,
     })
-})
+});
+
+
+
+
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+    const {prompt} = req.body;
+
+    console.log("Value of Prompt ::::: ", prompt)
+    
+    const response = await client.responses.create({
+        model: 'gpt-4o-mini',
+        input: prompt,
+        temperature: 0.2,
+        max_output_tokens: 100
+    });
+    res.json({
+        message: response.output_text
+    });
+});
 
 
 app.listen(3000, () => {
